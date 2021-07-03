7bfaa255d3f57f537aa7ed0f349aae8e3332e640
SimonHu <simonhu@192.168.208.129>
2021-07-03 15:06:07 +0800

    several updates

--- rookie/go_base/study-01.go
+++ rookie/go_base/study-01.go
@@ -5,7 +5,12 @@ import(
 	"os"
 )
 
+func init() {
+	fmt.Println("Program Start!")
+}
+
 func main()  {
+	fmt.Println("Hello World1")
 	var s, sep string
 	for i:=0; i<len(os.Args); i++ {
 		s += sep + os.Args[i]
